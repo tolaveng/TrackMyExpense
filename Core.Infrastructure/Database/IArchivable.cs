ab9c95a5352ccdb7fab63a858bfcb19c6e91e038
tolaveng <tolaveng@hotmail.com>
2021-12-01 20:06:55 +1100

    Upgrade to .net 6

---
+++ Core.Infrastructure/Database/IArchivable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Infrastructure.Database
+{
+    public interface IArchivable
+    {
+        bool Archived { get; set; }
+        DateTimeOffset? ArchivedAt { get; set; }
+    }
+}
