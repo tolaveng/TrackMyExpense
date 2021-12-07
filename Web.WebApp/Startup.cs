a5827462cfb3d2f8ca23942e9949b471480d221f
tolaveng <tolaveng@hotmail.com>
2021-12-07 20:19:05 +1100

    remove default blazor components

--- Web.WebApp/Startup.cs
+++ Web.WebApp/Startup.cs
@@ -13,7 +13,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Web.WebApp.Data;
 
 namespace Web.WebApp
 {
@@ -40,7 +39,6 @@ namespace Web.WebApp
 
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
